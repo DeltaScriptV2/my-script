a24c8b8794a479bd3f6d3a900cd47195c394e53b
DeltaScriptV2 <yhanmidoriya@gmail.com>
2026-03-15 07:57:17 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const scripts = {
+  StealaBrainrot: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/DeltaScriptV2/Free/refs/heads/main/IeakV2"))()',
+  AdoptMe: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/DeltaScriptV2/V.2/refs/heads/main/Adopt_Me"))()',
+  EscapeTsunamiForBrainrots: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/DeltaScriptV2/V2/refs/heads/main/EscapeTsunamiForBrainrots"))()'
+};
+
+function setScript(game, btn) {
+  document.getElementById("scriptBox").innerText = scripts[game];
+  document.querySelectorAll(".tab").forEach(t => t.classList.remove("active"));
+  btn.classList.add("active");
+}
+
+function copyScript() {
+  const text = document.getElementById("scriptBox").innerText;
+  navigator.clipboard.writeText(text).then(() => alert("✅ Script Copied!"));
+}
+function randomActiveUsers() {
+  const users = document.getElementById("activeUsers");
+  const min = 100; // minimum number of users
+  const max = 500; // maximum number of users
+  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  users.textContent = `👥 Active Users: ${randomNumber}`;
+}
+
+// Update every 10 seconds
+setInterval(randomActiveUsers, 2000);
