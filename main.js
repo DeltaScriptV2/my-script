18f499ac19be896e939037e760688a6a904ab85e
DeltaScriptV2 <yhanmidoriya@gmail.com>
2026-04-05 07:55:10 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const scripts = {
   MurderMystery2: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xentace/MM2/refs/heads/main/script20%25MM2"))()',
   StealaBrainrot: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/DeltaScriptV2/Free/refs/heads/main/IeakV2"))()',
-  AdoptMe: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/DeltaScriptV2/V.2/refs/heads/main/Adopt_Me"))()'
+  AdoptMe: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/DeltaScriptV2/V.2/refs/heads/main/Adopt_Me"))()',
+  SailorPiece: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/xentace/sailor/refs/heads/main/sailor%2520piece%2520piece"))()'
 };
 
 function setScript(game, btn) {
